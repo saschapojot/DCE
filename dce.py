486a4057d6a4e60fe3f4281f284fc9bb63edb231
Xi LIU <xi.liu@u.nus.edu>
2023-11-22 14:43:09 +0800

    using sparse matrix

--- dce.py
+++ dce.py
@@ -3,12 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
 import math
+
+import scipy.linalg
 from scipy.special import hermite
 from datetime import datetime
-from scipy.linalg import block_diag
 import copy
 from pathlib import Path
+from scipy import sparse
 # from scipy.linalg import ishermitian
+from scipy.sparse.linalg import inv
 
 j1H=1
 j2H=1
@@ -34,8 +37,8 @@ theta=np.pi/7
 Deltam=omegam-omegap
 
 tTot=0.1
-N1=500
-N2=500
+N1=50
+N2=50
 L1=5
 L2=5
 
@@ -50,16 +53,18 @@ x1ValsAll=np.array([-L1+dx1*n1 for n1 in range(0,N1)])
 x2ValsAll=np.array([-L2+dx2*n2 for n2 in range(0,N2)])
 
 #construct H0
-H0=(-1/2*omegac-1/2*Deltam)*np.eye(N1*N2,dtype=complex)
+IN1N2=sparse.eye(N1*N2,dtype=complex,format="csc")
+H0=(-1/2*omegac-1/2*Deltam)*IN1N2
 
-D2=np.diag(x2ValsAll)
+D2=sparse.diags(x2ValsAll,format="csc")
+S2=sparse.diags(np.power(x2ValsAll,2),format="csc")
+#diagD2=diag(D2,D2,...,D2)
+blockD2=[D2 for n1 in range(0,N1)]
+diagD2=sparse.block_diag(blockD2,format="csc",dtype=complex)
+#diagS2=diag(S2,S2,...,S2)
+blockS2=[S2 for n1 in range(0, N1)]
+diagS2=sparse.block_diag(blockS2,format="csc",dtype=complex)
 
-S2=np.diag(np.power(x2ValsAll,2))
-
-allD2=[D2 for n1 in range(0,N1)]
-allS2=[S2 for n1 in range(0,N1)]
-diagD2=block_diag(*allD2)#diagD2=diag(D2,D2,...,D2)
-diagS2=block_diag(*allS2)#diagS2=diag(S2,S2,...,S2)
 
 def H1Mat(j):
     """
@@ -67,22 +72,22 @@ def H1Mat(j):
     :param j: time  step j
     :return: value of matrix H1 at time step j
     """
-    tj=j*dt
-    return -1/2*g0*np.sqrt(2*omegam)*np.cos(omegap*(tj+1/2*dt))*diagD2
+    tj = j * dt
+    return -1 / 2 * g0 * np.sqrt(2 * omegam) * np.cos(omegap * (tj + 1 / 2 * dt)) * diagD2
+
 
 #construct H2
-IN2=np.eye(N2,dtype=complex)
+IN2=sparse.eye(N2,dtype=complex,format="csc")
 blockH2=[IN2*x1ValsAll[n1]**2 for n1 in range(0,N1)]
-diagH2=block_diag(*blockH2)
+diagH2=sparse.block_diag(blockH2,format="csc",dtype=complex)
 H2=1/2*omegac**2*diagH2
-
 
 #construct H3
 H3=(1/2*lmd*omegam*np.cos(theta)+1/2*Deltam*omegam)*diagS2
 
 
 blockH4=[D2*x1ValsAll[n1]**2 for n1 in range(0,N1)]
-diagH4=block_diag(*blockH4)
+diagH4=sparse.block_diag(blockH4,format="csc",dtype=complex)
 
 def H4Mat(j):
     """
@@ -92,18 +97,17 @@ def H4Mat(j):
     """
 
     tj=j*dt
+    return g0 * omegac * np.sqrt(2 * omegam) * np.cos(omegap * (tj + 1 / 2 * dt)) * diagH4
 
-    return g0*omegac*np.sqrt(2*omegam)*np.cos(omegap*(tj+1/2*dt))*diagH4
 
 
 P2Ones=np.ones(N2-1)
-P2=np.diag(P2Ones,k=1)+np.diag(-P2Ones,k=-1)
+P2=sparse.diags(P2Ones,offsets=1,format="csc",dtype=complex)\
+    +sparse.diags(-P2Ones, offsets=-1,format="csc",dtype=complex)
 
 blockH5=[P2 for n1 in range(0,N1)]
-blockA5IN2=[IN2 for n1 in range(0,N1)]
+diagH5=sparse.block_diag(blockH5,format="csc",dtype=complex)
 
-diagH5=block_diag(*blockH5)
-diagA5IN2=block_diag(*blockA5IN2)
 
 def H5Mat(j):
     """
@@ -111,11 +115,10 @@ def H5Mat(j):
     :param j: time  step j
     :return: value of matrix H5 at time step j
     """
-
     tj=j*dt
 
     A5j=-1j*g0*omegac*np.sqrt(2/omegam)*np.sin(omegap*(tj+1/2*dt))*diagH2\
-        +1j*1/2*g0*np.sqrt(2/omegam)*np.sin(omegap*(tj+1/2*dt))*diagA5IN2
+        +1j*1/2*g0*np.sqrt(2/omegam)*np.sin(omegap*(tj+1/2*dt))*IN1N2
 
     return A5j*1/(2*dx2)*diagH5
 
@@ -123,24 +126,33 @@ def H5Mat(j):
 
 #construct H6
 
-leftMat=np.diag(-2*np.ones(N1),k=0)+np.diag(np.ones(N1-1),k=1)+np.diag(np.ones(N1-1),k=-1)
+leftMat=sparse.diags(-2*np.ones(N1),offsets=0,format="csc",dtype=complex)\
+    +sparse.diags(np.ones(N1-1),offsets=1,format="csc",dtype=complex)\
+    +sparse.diags(np.ones(N1-1),offsets=-1,format="csc",dtype=complex)
 
-H6=-1/(2*dx1**2)*np.kron(leftMat,np.eye(N2,dtype=complex))
+
+
+
+H6=-1/(2*dx1**2)*sparse.kron(leftMat,sparse.eye(N2,dtype=complex,format="csc"),format="csc")
 
 
 #construct H7
 
-Q2=np.diag(-2*np.ones(N2),k=0)+np.diag(np.ones(N2-1),k=1)+np.diag(np.ones(N2-1),k=-1)
+Q2=sparse.diags(-2*np.ones(N2),offsets=0,format="csc",dtype=complex)\
+    +sparse.diags(np.ones(N2-1),offsets=1,format="csc",dtype=complex)\
+    +sparse.diags(np.ones(N2-1),offsets=-1,format="csc",dtype=complex)
+
 blockH7=[Q2 for n1 in range(0,N1)]
-diagH7=block_diag(*blockH7)
+
+diagH7=sparse.block_diag(blockH7,format="csc",dtype=complex)
 H7=(-Deltam/(2*omegam)+lmd*np.cos(theta)/(2*omegam))*1/(dx2**2)*diagH7
 
 #construct H8
 antiCommD2P2=D2@P2+P2@D2
-H8=1j*lmd*np.sin(theta)/(4*dx2)*np.kron(np.eye(N1,dtype=complex),antiCommD2P2)
+H8=1j*lmd*np.sin(theta)/(4*dx2)*sparse.kron(sparse.eye(N1,dtype=complex,format="csc"),antiCommD2P2)
 
 
-def psi(n1n2):
+def psi0(n1n2):
     """
 
     :param n1n2: [n1,n2], n1, n2 are positions in x1, x2 array
@@ -156,15 +168,11 @@ def psi(n1n2):
 
 
 n1n2All=[[n1,n2] for n1 in range(0,N1) for n2 in range(0,N2)]
-
 procNum=48
 
 pool0=Pool(procNum)
 tInitStart=datetime.now()
-ret0=pool0.map(psi,n1n2All)
-
-
-
+ret0=pool0.map(psi0,n1n2All)
 Psi0=np.zeros(N1*N2,dtype=complex)
 for item in ret0:
     n1,n2,val=item
@@ -175,34 +183,30 @@ tInitEnd=datetime.now()
 print("Initialization of Psi: ",tInitEnd-tInitStart)
 
 
-def eigj(j):
+def evoMat(j):
     """
 
     :param j: time step j
-    :return: eigenvalues and eigenvectors of HDRj
+    :return: (1-1/2 * i*dt *H)/(1+1/2*i*dt*H)
     """
+    HDRj = H0 + H1Mat(j) + H2 + H3 + H4Mat(j) + H5Mat(j) + H6 + H7 + H8
 
-    HDRj=H0+H1Mat(j)+H2+H3+H4Mat(j)+H5Mat(j)+H6+H7+H8
+    mat=(IN1N2-1/2*1j*dt*HDRj)@inv(IN1N2+1/2*1j*dt*HDRj)
 
-    vals,vecs=np.linalg.eigh(HDRj)
-    return [j,vals,vecs]
-
+    return [j,mat]
 
 
 
 tStepsAll=[j for j in range(0,M)]
-tEigStart=datetime.now()
-
+tInvStart=datetime.now()
 pool1=Pool(procNum)
+ret1=pool1.map(evoMat,tStepsAll)
 
-ret1=pool1.map(eigj,tStepsAll)
+tInvEnd=datetime.now()
 
-tEigEnd=datetime.now()
-
-print("eig time: ",tEigEnd-tEigStart)
+print("prod and inv time: ",tInvEnd-tInvStart)
 
 sortedRet1=sorted(ret1,key=lambda item: item[0])
-
 
 def oneStepEvolution(j,Psij):
     """
@@ -212,15 +216,10 @@ def oneStepEvolution(j,Psij):
     :return: wavefunction at time step j+1
     """
 
-    _,vals,vecs=sortedRet1[j]
-    PsiNext=np.zeros(N1*N2,dtype=complex)
-    for l in range(0,len(vals)):
-        El=vals[l]
-        vecl=vecs[:,l]
-        PsiNext+=(1-1/2*1j*dt*El)/(1+1/2*1j*dt*El)*np.vdot(vecl,Psij)*vecl
+    _,Uj=sortedRet1[j]
+    PsiNext=Uj@Psij
 
     return PsiNext
-
 
 
 tEvoStart=datetime.now()
@@ -238,88 +237,7 @@ print("evolution time: ",tEvoEnd-tEvoStart)
 
 outData=np.array(PsiAll).T
 dtFrm=pd.DataFrame(data=outData)
-
-
-
-
 outDirPrefix="./omegac"+str(omegac)+"omegam"+str(omegam)+"omegap"+str(omegap)+"er"+str(er)+"theta"+str(theta/np.pi)+"pi"+"/"
 Path(outDirPrefix).mkdir(parents=True, exist_ok=True)
 
 dtFrm.to_csv(outDirPrefix+"PsiAll.csv",index=False,header=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
